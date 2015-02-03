7827666e15e5447db2b344738e698dffcd3dfe93
Sunderlandkyl <sunderlandkyl@gmail.com>
2015-02-03 14:08:32 -0500

    Beginnings for the tilemap that will be used for movement and level generation.

---
+++ Assets/Scripts/TileData.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileData : MonoBehaviour {
+
+	public float x;
+	public float y;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SetLocation(float x, float y){
+
+		this.x = x;
+		this.y = y;
+		this.transform.localPosition = new Vector3(x, y, 0.0f);
+	}
+}
---
+++ Assets/Scripts/TilemapData.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TilemapData : MonoBehaviour {
+
+	public int x;
+	public int y;
+
+	public GameObject tileObject;
+
+	private ArrayList tiles;
+
+	// Use this for initialization
+	void Start () {
+	
+		tiles = new ArrayList();
+
+		for(int i=0; i < x; i++) {
+			for(int j=0; j < y; j++) {
+				GameObject tileTemp = Instantiate(tileObject) as GameObject;
+				tileTemp.transform.parent = this.transform;
+				tileTemp.GetComponent<TileData>().SetLocation(i, j);
+				tiles.Add (tileTemp);
+
+			}
+
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
