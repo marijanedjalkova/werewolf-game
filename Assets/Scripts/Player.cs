49307cee757e92974c567b69c2df4a8398ab4528
Sunderlandkyl <Sunderlandkyl@gmail.com>
2015-03-03 15:48:17 -0500

    Adding Alex's changes.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -20,11 +20,11 @@ public class Player : MonoBehaviour {
 
 		anim = GetComponent<Animator> ();
 		anim.SetBool ("wolfForm", false);
-		//anim = GetComponent<Animator> ();
-		//anim.SetBool ("up", false);
-		///anim.SetBool ("down", false);
-		//anim.SetBool ("left", false);
-		//anim.SetBool ("right", false);
+		anim.SetBool ("up", false);
+		anim.SetBool ("down", false);
+		anim.SetBool ("left", false);
+		anim.SetBool ("right", false);
+		anim.SetBool ("Move", false);
 	}
 	
 	void FixedUpdate(){
@@ -52,49 +52,56 @@ public class Player : MonoBehaviour {
 			}
 		}
 
-		//bool move = false; 
-		velocity = new Vector2(0.0f, 0.0f);
+			bool move = false; 
+			velocity = new Vector2(0.0f, 0.0f);
 		if(Input.GetKey(KeyCode.D)){
-			//move = true;
-		//	anim.SetBool ("up", false);
-		//	anim.SetBool ("down", false);
-		//	anim.SetBool ("left", false);
-		//	anim.SetBool("right", true);
+			move = true;
+			anim.SetBool ("up", false);
+			anim.SetBool ("down", false);
+			anim.SetBool ("left", false);
+			anim.SetBool("right", true);
 			velocity += new Vector2(playerSpeed, 0.0f);
 		}
 
 		if(Input.GetKey(KeyCode.A)){
-			//move = true;
-		//	anim.SetBool ("up", false);
-		//	anim.SetBool ("down", false);
-		//	anim.SetBool ("right", false);
-		//	anim.SetBool ("left", true);
+			move = true;
+			anim.SetBool ("up", false);
+			anim.SetBool ("down", false);
+			anim.SetBool ("right", false);
+			anim.SetBool ("left", true);
 			velocity -= new Vector2(playerSpeed, 0.0f);
 		}
 		if(Input.GetKey(KeyCode.W)){
-			//move = true;
-		//	anim.SetBool ("down", false);
-		//	anim.SetBool ("left", false);
-		//	anim.SetBool ("right", false);
-		//	anim.SetBool ("up", true);
+			move = true;
+			anim.SetBool ("down", false);
+			anim.SetBool ("left", false);
+			anim.SetBool ("right", false);
+			anim.SetBool ("up", true);
 			velocity += new Vector2( 0.0f, playerSpeed);
 		}
 		if(Input.GetKey(KeyCode.S)){
-			//move = true;
-		//	anim.SetBool ("up", false);
-		//	anim.SetBool ("left", false);
-		//	anim.SetBool ("right", false);
-		//	anim.SetBool ("down", true);
+			move = true;
+			anim.SetBool ("up", false);
+			anim.SetBool ("left", false);
+			anim.SetBool ("right", false);
+			anim.SetBool ("down", true);
 			velocity -= new Vector2(0.0f, playerSpeed);
 		}
 
-		//anim.SetBool("moving", move);
+		anim.SetBool("Move", move);
+
 		if (velocity.magnitude > 0){
 			rigidbody2D.velocity = this.velocity;
 		} else {
 			rigidbody2D.Sleep ();
 		}
-		
+
+
+		//Finds distance to silver
+		var silver = GameObject.Find ("Silver");
+		if (silver != null){
+			silverDamage (silver);
+		}
 	}	
 	
 	void TransformNPCCheck(){
@@ -125,5 +132,20 @@ public class Player : MonoBehaviour {
 
 	}
 
-
+	//Determines if the player is close enough to specified silver to deal damage, and carries it out
+	void silverDamage(GameObject silver){
+		var health = GameObject.Find ("Health");
+		float distance = Vector3.Distance (this.transform.position, silver.transform.position);
+		var healthScript = health.GetComponent<Health>();
+		Debug.Log(healthScript.current_health);
+		if (distance < 1) {
+			healthScript.decreaseBy (1.0f);
+		} else if (distance < 2) {
+			healthScript.decreaseBy (0.5f);
+		} else if (distance < 3) {
+			healthScript.decreaseBy (0.25f);
+		} else if (distance < 5) {
+			healthScript.decreaseBy (0.1f);
+		}
+	}
 }
